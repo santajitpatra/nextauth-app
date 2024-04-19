dcf54db0fcef99d3e2cc218b9bc35d52dc69615e
Santajit Patra <121734312+santajitpatra@users.noreply.github.com>
2024-04-19 23:59:15 +0530

    feat(auth): Implement registration form with validation and feedback
    feat(auth): Implement registration form with validation and feedback
    
    * **RegisterForm.tsx (new):**
      - Creates a registration form with fields for email, password, confirmation password, first name, and last name.
      - Utilizes `useForm` and `zod` for form state management and validation.
      - Integrates `FormError` and `FormSuccess` components for dynamic feedback.
      - Handles form submission using `login` action with feedback transitions.
    
    Provides a registration form with validation, error/success messages, and leverages the existing login functionality.

---
+++ components/auth/RegisterForm.tsx
@@ -0,0 +1,164 @@
+"use client";
+
+import { CardWrapper } from "./CardWrapper";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { RegisterFormSchema } from "@/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { FormError } from "./FormError";
+import { FormSuccess } from "./FormSuccess";
+import { login } from "@/actions/login";
+import { useState, useTransition } from "react";
+
+export const RegisterForm = () => {
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof RegisterFormSchema>>({
+    resolver: zodResolver(RegisterFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      firstName: "",
+      lastName: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof RegisterFormSchema>) => {
+    setError("");
+    setSuccess("");
+
+    startTransition(() => {
+      login(values).then((response) => {
+        if (response.error) {
+          setError(response.error);
+        } else {
+          setSuccess(response.success);
+        }
+      });
+    });
+  };
+
+  return (
+    <CardWrapper
+      headerLable="Create an account"
+      backButtonLabel="Already have an account?"
+      backButtonHref="/auth/login"
+      showSocialLogin
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="firstName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>First Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      type="text"
+                      placeholder="john"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Last Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      type="text"
+                      placeholder="Doe"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      type="email"
+                      placeholder="example@domain.com"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      type="password"
+                      placeholder="********"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      type="password"
+                      placeholder="********"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button disabled={isPending} type="submit" className="w-full">
+            Create an account
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+};
--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,27 @@ export const LoginFormSchema = z.object({
     message: "Password must be at least 6 characters",
   }),
 });
+
+export const RegisterFormSchema = z.object({
+  email: z.string().email({
+    message: "Please enter a valid email address",
+  }),
+  password: z.string().min(4, {
+    message: "Password must be at least 6 characters",
+  }),
+  confirmPassword: z.string().min(4, {
+    message: "Password must be at least 6 characters",
+  }),
+  firstName: z.string().min(2, {
+    message: "First name must be at least 2 characters",
+  })
+  .max(50, {
+    message: "First name must be less than 50 characters",
+  }),
+  lastName: z.string().min(2, {
+    message: "Last name must be at least 2 characters",
+  })
+   .max(50, {
+    message: "Last name must be less than 50 characters",
+  }),
+});
